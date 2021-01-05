9c55e94a4e6127a387a644f25e278a6783687562
Frazer Smith <frazerry@outlook.com>
2021-01-05 12:20:37 +0000

    refactor(routes): use fluent json schema for other objects

--- src/routes/documents/schema.js
+++ src/routes/documents/schema.js
@@ -15,129 +15,105 @@ const headerSchema = S.object().prop(
 const registerGetSchema = {
 	description: "Retrieve document metadata from register",
 	headers: headerSchema,
-	querystring: {
-		type: "object",
-		properties: {
-			lastModified: {
-				description: "Last modified datetime of document",
-				examples: [
+
+	querystring: S.object()
+		.prop(
+			"lastModified",
+			S.string()
+				.description("Last modified datetime of document")
+				.examples([
 					"2021-01-01",
 					"ge2021-01-01T00:00:01",
 					"ge2021-01-01",
 					"2021-01-01T00:00:01",
-				],
-				type: "string",
-				pattern:
-					"^(?:eq|ne|ge|le|gt|lt|sa|eb|ap|)\\d{4}-\\d{2}-\\d{2}(?:T\\d{2}\\:\\d{2}\\:\\d{2}|)$",
-			},
-			page: {
-				description: "Page to retrieve",
-				default: "1",
-				examples: ["1", "10"],
-				type: "string",
-				pattern: "^\\d{1,}$",
-			},
-			perPage: {
-				description: "Number of documents to return per page",
-				default: "1",
-				examples: ["1", "10"],
-				type: "string",
-				pattern: "^\\d{1,}$",
-			},
-		},
-		required: ["lastModified"],
-	},
+				])
+				.pattern(
+					"^(?:eq|ne|ge|le|gt|lt|sa|eb|ap|)\\d{4}-\\d{2}-\\d{2}(?:T\\d{2}\\:\\d{2}\\:\\d{2}|)$"
+				)
+				.required()
+		)
+		.prop(
+			"page",
+			S.number()
+				.description("Page to retrieve")
+				.default(1)
+				.examples([1, 10])
+		)
+		.prop(
+			"perPage",
+			S.number()
+				.description("Number of documents to return per page")
+				.default(1)
+				.examples([1, 10])
+		),
 	response: {
-		200: {
-			type: "object",
-			properties: {
-				data: {
-					type: "array",
-					items: {
-						type: "object",
-						properties: {
-							GUID: {
-								examples: ["EXAMPLE-GUID"],
-								type: "string",
-							},
-							fhir_id: {
-								examples: ["99999"],
-								type: "number",
-							},
-							Title: {
-								examples: [
-									"99999   DUCK 11 July 2015 11 27.pdf",
-								],
-								type: "string",
-							},
-							Clinic: {
-								examples: ["CLO/BIA"],
-								type: "string",
-							},
-							Document_Type: {
-								examples: ["Clinic Letter"],
-								type: "string",
-							},
-							Filesname: {
-								examples: [
-									"99999   DUCK 11 July 2015 11 27.pdf",
-								],
-								type: "string",
-							},
-							URL: {
-								examples: [
+		200: S.object()
+			.prop(
+				"data",
+				S.array().items(
+					S.object()
+						.prop("guid", S.string().examples(["EXAMPLE-GUID"]))
+						.prop("fhir_id", S.number().examples(["99999"]))
+						.prop(
+							"title",
+							S.string().examples([
+								"99999   DUCK 11 July 2015 11 27.pdf",
+							])
+						)
+						.prop(
+							"specialty",
+							S.string().examples(["General Surgery"])
+						)
+						.prop("clinic", S.string().examples(["CLO/BIA"]))
+						.prop(
+							"document_type",
+							S.string().examples(['"Clinic Letter"'])
+						)
+						.prop(
+							"file_name",
+							S.string().examples([
+								"99999   DUCK 11 July 2015 11 27.pdf",
+							])
+						)
+						.prop(
+							"url",
+							S.string()
+								.examples([
 									"https://notreal.ydh.nhs.uk/sites/MedicalRecords1/_layouts/15/DocIdRedir.aspx?ID=EXAMPLE-GUID",
-								],
-								type: "string",
-							},
-							CreatedDate: {
-								examples: ["2015-09-30T05:40:14.000Z"],
-								format: "date-time",
-								type: "string",
-							},
-							Modified: {
-								examples: ["2020-08-10T03:51:54.000Z"],
-								format: "date-time",
-								type: "string",
-							},
-							Specialty: {
-								examples: ["General Surgery"],
-								type: "string",
-							},
-						},
-					},
-				},
-
-				meta: {
-					type: "object",
-					properties: {
-						pagination: {
-							type: "object",
-							properties: {
-								total: {
-									examples: [0, 1, 10],
-									type: "number",
-								},
-								per_page: {
-									default: 1,
-									examples: [1, 10],
-									type: "number",
-								},
-								current_page: {
-									default: 1,
-									examples: [1, 10],
-									type: "number",
-								},
-								total_pages: {
-									examples: [1, 10],
-									type: "number",
-								},
-							},
-						},
-					},
-				},
-			},
-		},
+								])
+								.format("uri")
+						)
+						.prop(
+							"created_date",
+							S.string()
+								.examples(["2015-09-30T05:40:14.000Z"])
+								.format("date-time")
+						)
+						.prop(
+							"modified",
+							S.string()
+								.examples(["2020-08-10T03:51:54.000Z"])
+								.format("date-time")
+						)
+				)
+			)
+			.prop(
+				"meta",
+				S.object().prop(
+					"pagination",
+					S.object()
+						.prop("total", S.number().examples([0, 1, 10]))
+						.prop(
+							"per_page",
+							S.number().default(1).examples([1, 10])
+						)
+						.prop(
+							"current_page",
+							S.number().default(1).examples([1, 10])
+						)
+						.prop("total_pages", S.number().examples([1, 10]))
+				)
+			),
 	},
 };
 
@@ -145,61 +121,48 @@ const registerGetSchema = {
 const receiptDeleteSchema = {
 	description: "Delete document read receipt",
 	headers: headerSchema,
-	params: {
-		type: "object",
-		properties: {
-			id: {
-				description: "Logical id of the artifact",
-				examples: ["EXAMPLE-GUID"],
-				type: "string",
-			},
-		},
-		required: ["id"],
-	},
-	querystring: {
-		type: "object",
-		properties: {
-			patientId: {
-				description: "Unique patient identifier",
-				examples: [9999999999],
-				type: "number",
-			},
-		},
-		required: ["patientId"],
-	},
+	params: S.object().prop(
+		"id",
+		S.string()
+			.description("Logical id of the artifact")
+			.examples(["EXAMPLE-GUID"])
+			.required()
+	),
+	querystring: S.object().prop(
+		"patientId",
+		S.number()
+			.description("Unique patient identifier")
+			.examples([9999999999])
+			.required()
+	),
 };
 
 // TODO: Add 200 and 300 response schema
 const receiptPutSchema = {
 	description: "Create document read receipt",
 	headers: headerSchema,
-	params: {
-		type: "object",
-		properties: {
-			id: {
-				description: "Logical id of the artifact",
-				examples: ["EXAMPLE-GUID"],
-				type: "string",
-			},
-		},
-		required: ["id"],
-	},
-	querystring: {
-		type: "object",
-		properties: {
-			patientId: {
-				description: "Unique patient identifier",
-				examples: [9999999999],
-				type: "number",
-			},
-			timestamp: {
-				description: "Read time of document",
-				format: "date-time",
-				type: "string",
-			},
-		},
-		required: ["patientId", "timestamp"],
-	},
+	params: S.object().prop(
+		"id",
+		S.string()
+			.description("Logical id of the artifact")
+			.examples(["EXAMPLE-GUID"])
+			.required()
+	),
+	querystring: S.object()
+		.prop(
+			"patientId",
+			S.number()
+				.description("Unique patient identifier")
+				.examples([9999999999])
+				.required()
+		)
+		.prop(
+			"timestamp",
+			S.string()
+				.description("Read time of document")
+				.format("date-time")
+				.required()
+		),
 };
 
 module.exports = { registerGetSchema, receiptDeleteSchema, receiptPutSchema };
--- src/routes/preferences/schema.js
+++ src/routes/preferences/schema.js
@@ -17,79 +17,66 @@ const optionsGetSchema = {
 		"Retrieve default available list of patient contact preferences that can be set",
 	headers: headerSchema,
 	response: {
-		200: {
-			type: "object",
-			properties: {
-				preferences: {
-					type: "array",
-					items: {
-						type: "object",
-						properties: {
-							type: {
-								type: "object",
-								properties: {
-									display: {
-										type: "string",
-									},
-									id: {
-										type: "number",
-									},
-									priority: {
-										type: "number",
-									},
-									selected: {
-										type: "number",
-									},
-									options: {
-										type: "array",
-										items: {
-											type: "object",
-											properties: {
-												display: {
-													type: "string",
-												},
-												value: {
-													type: "number",
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
+		200: S.object().prop(
+			"preferences",
+			S.array().items(
+				S.object().prop(
+					"type",
+					S.object()
+						.prop(
+							"display",
+							S.string().enum([
+								"SMS",
+								"Email",
+								"Phone",
+								"Letters",
+							])
+						)
+						.prop("id", S.number().enum([1, 2, 3, 4]))
+						.prop("priority", S.number().enum([0, 1, 2, 3]))
+						.prop("selected", S.number().enum([1, 2]))
+						.prop("display", S.string().required())
+						.prop("value", S.number(1).required())
+						.prop(
+							"options",
+							S.array().items(
+								S.object()
+									.prop(
+										"display",
+										S.string().enum(["yes", "no"])
+									)
+									.prop("value", S.number().enum([1, 2]))
+							)
+						)
+				)
+			)
+		),
 	},
 };
 
+// TODO: Add 200 and 300 response schema
 const userGetSchema = {
-	params: {
-		type: "object",
-		properties: {
-			id: {
-				description: "Unique patient identifier",
-				examples: [1],
-				type: "number",
-			},
-		},
-		required: ["id"],
-	},
+	params: S.object().prop(
+		"id",
+		S.number().description("Unique patient identifier").examples([1])
+	),
 };
 
+// TODO: Add 200 and 300 response schema
 const userPutSchema = {
-	params: {
-		type: "object",
-		properties: {
-			id: {
-				description: "Unique patient identifier",
-				examples: [1],
-				type: "number",
-			},
-		},
-		required: ["id"],
-	},
+	params: S.object().prop(
+		"id",
+		S.number().description("Unique patient identifier").examples([1])
+	),
+	body: S.object().prop(
+		"preferences",
+		S.array().items(
+			S.object()
+				.prop("id", S.number().required())
+				.prop("priority", S.number().required())
+				.prop("selected", S.number().required())
+		)
+	),
 };
 
 module.exports = { optionsGetSchema, userGetSchema, userPutSchema };
