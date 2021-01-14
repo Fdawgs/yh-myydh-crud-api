83e0e0a6a083f1fb1e1d56ff65e5d8d43977b6a3
Frazer Smith <frazerry@outlook.com>
2021-01-14 10:55:59 +0000

    fix(server): set referrer policy with fallback

--- src/server.js
+++ src/server.js
@@ -43,6 +43,9 @@ async function plugin(server, config) {
 					),
 				},
 			},
+			referrerPolicy: {
+				policy: ["no-referrer", "strict-origin-when-cross-origin"],
+			},
 		}))
 
 		.register(healthCheck)
