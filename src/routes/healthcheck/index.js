51d6db9b518da10e002361ca134af6908e5183d3
Frazer Smith <frazer.dev@outlook.com>
2021-07-13 14:15:33 +0100

    refactor(routes/healthcheck): move `Accept` header handling back to hook

--- src/routes/healthcheck/index.js
+++ src/routes/healthcheck/index.js
@@ -10,19 +10,22 @@ const { healthcheckGetSchema } = require("./schema");
  * @param {Function} server - Fastify instance.
  */
 async function route(server) {
+	server.addHook("onRequest", async (req, res) => {
+		if (
+			// Catch unsupported Accept header media types
+			!healthcheckGetSchema.produces.includes(
+				req.accepts().type(healthcheckGetSchema.produces)
+			)
+		) {
+			res.send(NotAcceptable());
+		}
+	});
+
 	server.route({
 		method: "GET",
 		url: "/healthcheck",
 		schema: healthcheckGetSchema,
 		async handler(req, res) {
-			if (
-				// Catch unsupported Accept header media types
-				!healthcheckGetSchema.produces.includes(
-					req.accepts().type(healthcheckGetSchema.produces)
-				)
-			) {
-				res.send(NotAcceptable());
-			}
 			res.send("ok");
 		},
 	});
