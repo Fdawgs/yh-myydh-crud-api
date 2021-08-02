79b4392eb9199ceb57558e4280467262cd60f62e
Frazer Smith <frazer.dev@outlook.com>
2021-08-02 08:07:40 +0100

    chore!: increase minimum required version of node from 12 to 14
    BREAKING CHANGE: minimum required version of node increased from 12 to 14 to allow for new ECMAScript syntax to be used

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	env: {
-		es2021: true,
+		es2020: true,
 		node: true,
 	},
 	extends: [
@@ -12,6 +12,7 @@ module.exports = {
 		"prettier",
 	],
 	parserOptions: {
+		ecmaVersion: 2020,
 		sourceType: "module",
 		ecmaFeatures: {
 			impliedStrict: true,
