fecfb355ca80b57f9034ba4c6bedda2d6796e411
Frazer Smith <frazer.dev@outlook.com>
2021-08-04 12:43:39 +0100

    test(routes/preferences/options): sort tests alphabetically ascending

--- src/routes/preferences/options/route.test.js
+++ src/routes/preferences/options/route.test.js
@@ -3,102 +3,6 @@ const plugin = require(".");
 const getConfig = require("../../../config");
 
 describe("Options Route", () => {
-	describe("PostgreSQL Database Backend", () => {
-		describe("GET Requests", () => {
-			let options;
-			let server;
-
-			beforeAll(async () => {
-				options = await getConfig();
-				options.database.client = "postgresql";
-
-				server = Fastify();
-				server.register(plugin, options);
-
-				await server.ready();
-			});
-
-			afterAll(async () => {
-				await server.close();
-			});
-
-			test("Should return preference options", async () => {
-				const mockQueryFn = jest.fn().mockResolvedValue([
-					{
-						rows: [
-							{
-								preferenceTypeId: 1,
-								preferenceTypeDisplay: "SMS",
-							},
-						],
-					},
-					{
-						rows: [
-							{
-								preferenceTypeId: 1,
-								preferenceTypeDisplay: "SMS",
-								preferenceOptionDisplay: "yes",
-								preferenceOptionValue: 1,
-							},
-							{
-								preferenceTypeId: 1,
-								preferenceTypeDisplay: "SMS",
-								preferenceOptionDisplay: "no",
-								preferenceOptionValue: 2,
-							},
-						],
-					},
-				]);
-
-				server.db = {
-					query: mockQueryFn,
-				};
-
-				const response = await server.inject({
-					method: "GET",
-					url: "/",
-				});
-
-				expect(mockQueryFn).toHaveBeenCalledTimes(1);
-				expect(response.statusCode).toEqual(200);
-			});
-
-			test("Should return HTTP status code 404 if no values returned from database", async () => {
-				const mockQueryFn = jest.fn().mockResolvedValue([{}, {}]);
-
-				server.db = {
-					query: mockQueryFn,
-				};
-
-				const response = await server.inject({
-					method: "GET",
-					url: "/",
-				});
-
-				expect(mockQueryFn).toHaveBeenCalledTimes(1);
-				expect(response.statusCode).toEqual(404);
-			});
-
-			test("Should return HTTP status code 500 if connection issue encountered", async () => {
-				const mockQueryFn = jest
-					.fn()
-					.mockRejectedValue(Error("Failed to connect to DB"));
-
-				server.db = {
-					query: mockQueryFn,
-				};
-
-				const response = await server.inject({
-					method: "GET",
-					url: "/",
-				});
-
-				expect(mockQueryFn).toHaveBeenCalledTimes(1);
-				expect(response.statusCode).toEqual(500);
-			});
-		});
-	});
-
 	describe("MSSQL Database Backend", () => {
 		describe("GET Requests", () => {
 			let options;
@@ -161,6 +65,102 @@ describe("Options Route", () => {
 				const mockQueryFn = jest.fn().mockResolvedValue({
 					recordsets: [[], []],
 				});
+
+				server.db = {
+					query: mockQueryFn,
+				};
+
+				const response = await server.inject({
+					method: "GET",
+					url: "/",
+				});
+
+				expect(mockQueryFn).toHaveBeenCalledTimes(1);
+				expect(response.statusCode).toEqual(404);
+			});
+
+			test("Should return HTTP status code 500 if connection issue encountered", async () => {
+				const mockQueryFn = jest
+					.fn()
+					.mockRejectedValue(Error("Failed to connect to DB"));
+
+				server.db = {
+					query: mockQueryFn,
+				};
+
+				const response = await server.inject({
+					method: "GET",
+					url: "/",
+				});
+
+				expect(mockQueryFn).toHaveBeenCalledTimes(1);
+				expect(response.statusCode).toEqual(500);
+			});
+		});
+	});
+
+	describe("PostgreSQL Database Backend", () => {
+		describe("GET Requests", () => {
+			let options;
+			let server;
+
+			beforeAll(async () => {
+				options = await getConfig();
+				options.database.client = "postgresql";
+
+				server = Fastify();
+				server.register(plugin, options);
+
+				await server.ready();
+			});
+
+			afterAll(async () => {
+				await server.close();
+			});
+
+			test("Should return preference options", async () => {
+				const mockQueryFn = jest.fn().mockResolvedValue([
+					{
+						rows: [
+							{
+								preferenceTypeId: 1,
+								preferenceTypeDisplay: "SMS",
+							},
+						],
+					},
+					{
+						rows: [
+							{
+								preferenceTypeId: 1,
+								preferenceTypeDisplay: "SMS",
+								preferenceOptionDisplay: "yes",
+								preferenceOptionValue: 1,
+							},
+							{
+								preferenceTypeId: 1,
+								preferenceTypeDisplay: "SMS",
+								preferenceOptionDisplay: "no",
+								preferenceOptionValue: 2,
+							},
+						],
+					},
+				]);
+
+				server.db = {
+					query: mockQueryFn,
+				};
+
+				const response = await server.inject({
+					method: "GET",
+					url: "/",
+				});
+
+				expect(mockQueryFn).toHaveBeenCalledTimes(1);
+				expect(response.statusCode).toEqual(200);
+			});
+
+			test("Should return HTTP status code 404 if no values returned from database", async () => {
+				const mockQueryFn = jest.fn().mockResolvedValue([{}, {}]);
 
 				server.db = {
 					query: mockQueryFn,
