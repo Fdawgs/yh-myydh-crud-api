f139fd5a2b83037b83f8ff917bba4020b058f9d6
Frazer Smith <frazer.dev@outlook.com>
2021-09-14 06:39:24 +0000

    feat(config): support HTTP/2 via `HTTPS_HTTP2_ENABLED` env variable (#405)

--- src/config/config.test.js
+++ src/config/config.test.js
@@ -35,6 +35,7 @@ describe("Configuration", () => {
 		const HTTPS_SSL_CERT_PATH =
 			"./test_resources/test_ssl_cert/server.cert";
 		const HTTPS_SSL_KEY_PATH = "./test_resources/test_ssl_cert/server.key";
+		const HTTPS_HTTP2_ENABLED = true;
 		const LOG_LEVEL = faker.random.arrayElement([
 			"debug",
 			"warn",
@@ -71,6 +72,7 @@ describe("Configuration", () => {
 			CORS_MAX_AGE,
 			HTTPS_SSL_CERT_PATH,
 			HTTPS_SSL_KEY_PATH,
+			HTTPS_HTTP2_ENABLED,
 			LOG_LEVEL,
 			LOG_ROTATION_DATE_FORMAT,
 			LOG_ROTATION_FILENAME,
@@ -120,9 +122,11 @@ describe("Configuration", () => {
 		);
 
 		expect(config.fastifyInit.https).toEqual({
+			allowHTTP1: true,
 			cert: expect.any(Buffer),
 			key: expect.any(Buffer),
 		});
+		expect(config.fastifyInit.http2).toEqual(true);
 
 		expect(config.cors).toEqual({
 			origin: CORS_ORIGIN,
@@ -163,9 +167,9 @@ describe("Configuration", () => {
 		const CORS_ALLOW_CREDENTIALS = "";
 		const CORS_EXPOSED_HEADERS = "";
 		const CORS_MAX_AGE = "";
-		const HTTPS_SSL_CERT_PATH =
-			"./test_resources/test_ssl_cert/server.cert";
-		const HTTPS_SSL_KEY_PATH = "./test_resources/test_ssl_cert/server.key";
+		const HTTPS_SSL_CERT_PATH = "";
+		const HTTPS_SSL_KEY_PATH = "";
+		const HTTPS_HTTP2_ENABLED = "";
 		const LOG_LEVEL = "";
 		const LOG_ROTATION_DATE_FORMAT = "";
 		const LOG_ROTATION_FILENAME = "./test_resources/test_log";
@@ -197,6 +201,7 @@ describe("Configuration", () => {
 			CORS_MAX_AGE,
 			HTTPS_SSL_CERT_PATH,
 			HTTPS_SSL_KEY_PATH,
+			HTTPS_HTTP2_ENABLED,
 			LOG_LEVEL,
 			LOG_ROTATION_DATE_FORMAT,
 			LOG_ROTATION_FILENAME,
@@ -245,10 +250,8 @@ describe("Configuration", () => {
 			',"time"'
 		);
 
-		expect(config.fastifyInit.https).toEqual({
-			cert: expect.any(Buffer),
-			key: expect.any(Buffer),
-		});
+		expect(config.fastifyInit.https).toBeUndefined();
+		expect(config.fastifyInit.http2).toBeUndefined();
 
 		expect(config.cors).toEqual({
 			origin: false,
@@ -291,6 +294,7 @@ describe("Configuration", () => {
 		const HTTPS_PFX_FILE_PATH =
 			"./test_resources/test_ssl_cert/server.cert"; // I know it's not an actual PFX file
 		const HTTPS_PFX_PASSPHRASE = faker.lorem.word();
+		const HTTPS_HTTP2_ENABLED = true;
 		const LOG_LEVEL = faker.random.arrayElement([
 			"debug",
 			"warn",
@@ -307,6 +311,7 @@ describe("Configuration", () => {
 			CORS_MAX_AGE,
 			HTTPS_PFX_FILE_PATH,
 			HTTPS_PFX_PASSPHRASE,
+			HTTPS_HTTP2_ENABLED,
 			LOG_LEVEL,
 		});
 
@@ -318,9 +323,11 @@ describe("Configuration", () => {
 		});
 
 		expect(config.fastifyInit.https).toEqual({
+			allowHTTP1: true,
 			passphrase: HTTPS_PFX_PASSPHRASE,
 			pfx: expect.any(Buffer),
 		});
+		expect(config.fastifyInit.http2).toEqual(true);
 
 		expect(config.cors).toEqual({
 			credentials: CORS_ALLOW_CREDENTIALS,
