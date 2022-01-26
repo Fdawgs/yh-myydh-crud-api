4204ba95a2e18c63b59ad2d3478427e1e0204cb6
Frazer Smith <frazer.dev@outlook.com>
2022-01-26 10:12:40 +0000

    fix(plugins/db): log error if connection fails

--- src/plugins/db/index.js
+++ src/plugins/db/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable security-node/detect-crlf */
 const fp = require("fastify-plugin");
 
 const { Pool } = require("pg");
@@ -13,36 +14,50 @@ const mssql = require("mssql");
  * Example connection string: `mssql://username:password@localhost/database`.
  */
 async function plugin(server, options) {
-	switch (options.client.toLowerCase()) {
-		case "postgresql":
-			server.log.info("Connecting to PostgreSQL DB");
-			// eslint-disable-next-line no-case-declarations
-			const pool = new Pool({
-				connectionString: options.connection,
-			});
-			server.log.info("PostgreSQL DB connection opened");
+	try {
+		switch (options.client.toLowerCase()) {
+			case "postgresql":
+				server.log.info("Connecting to PostgreSQL DB");
+				// eslint-disable-next-line no-case-declarations
+				const pool = new Pool({
+					connectionString: options.connection,
+				});
 
-			server.decorate("db", pool);
-			server.addHook("onClose", async () => {
-				server.log.info("Closing PostgreSQL DB connection");
-				await pool.end();
-				server.log.info("PostgreSQL DB connection closed");
-			});
+				/**
+				 * Attempt to connect to DB and check connection string
+				 * is valid, will throw error if not
+				 */
+				await pool.query("SELECT '1' AS \"connection_test\"");
 
-			break;
-		case "mssql":
-		default:
-			server.log.info("Connecting to MSSQL DB");
-			await mssql.connect(options.connection);
-			server.log.info("MSSQL DB connection opened");
+				server.log.info("PostgreSQL DB connection opened");
 
-			server.decorate("db", mssql);
-			server.addHook("onClose", async () => {
-				server.log.info("Closing MSSQL DB connection");
-				await mssql.close();
-				server.log.info("MSSQL DB connection closed");
-			});
-			break;
+				server.decorate("db", pool);
+				server.addHook("onClose", async () => {
+					server.log.info("Closing PostgreSQL DB connection");
+					await pool.end();
+					server.log.info("PostgreSQL DB connection closed");
+				});
+
+				break;
+			case "mssql":
+			default:
+				server.log.info("Connecting to MSSQL DB");
+				await mssql.connect(options.connection);
+				server.log.info("MSSQL DB connection opened");
+
+				server.decorate("db", mssql);
+				server.addHook("onClose", async () => {
+					server.log.info("Closing MSSQL DB connection");
+					await mssql.close();
+					server.log.info("MSSQL DB connection closed");
+				});
+				break;
+		}
+	} catch (err) /* istanbul ignore next */ {
+		server.log.error(
+			`Unable to connect to ${options.client} DB instance: ${err}`
+		);
+		process.exit(1);
 	}
 }
 
