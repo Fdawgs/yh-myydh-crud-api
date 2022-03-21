0cd2ffda54784bc5ab5cddb316927e5e5d7de3a1
Frazer Smith <frazer.dev@outlook.com>
2022-03-21 14:34:53 +0000

    feat(routes/admin/access): add `location` header to 201 responses

--- src/routes/admin/access/bearer-token/route.test.js
+++ src/routes/admin/access/bearer-token/route.test.js
@@ -574,6 +574,11 @@ describe("Access Route", () => {
 							scopes: testPayload.scopes,
 						},
 					});
+					expect(response.headers).toMatchObject({
+						location: expect.stringContaining(
+							`/admin/access/bearer-token/${testId}`
+						),
+					});
 					expect(response.statusCode).toBe(201);
 				});
 
