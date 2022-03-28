a144094d40b92871236f55d69ae0c13cf6bd3272
Frazer Smith <frazer.dev@outlook.com>
2022-03-28 08:55:30 +0100

    fix(routes/docs/json): add cors support

--- src/routes/docs/json/index.js
+++ src/routes/docs/json/index.js
@@ -1,11 +1,24 @@
+// Import plugins
+const cors = require("fastify-cors");
+
 const { docsJsonGetSchema } = require("./schema");
 
 /**
  * @author Frazer Smith
  * @description Sets routing options for server.
  * @param {object} server - Fastify instance.
+ * @param {object} options - Route config values.
+ * @param {object} options.cors - CORS settings.
  */
-async function route(server) {
+async function route(server, options) {
+	// Register plugins
+	server
+		// Enable CORS if options passed
+		.register(cors, {
+			...options.cors,
+			methods: ["GET"],
+		});
+
 	server.route({
 		method: "GET",
 		url: "/",
