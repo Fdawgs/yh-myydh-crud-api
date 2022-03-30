2e56e0d55e8109c33805bb0053a320e53b587fbf
Frazer Smith <frazer.dev@outlook.com>
2022-03-30 08:44:26 +0100

    refactor(plugins/serialize-json-to-xml): target application/json only

--- src/plugins/serialize-json-to-xml/index.js
+++ src/plugins/serialize-json-to-xml/index.js
@@ -5,19 +5,19 @@ const secJSON = require("secure-json-parse");
 /**
  * @author Frazer Smith
  * @description On-send plugin that adds support for serialization
- * of JSON responses, allowing them to be returned as XML if the
- * accepts request header explicitly includes the "application/xml"
- * media type before "application/json".
+ * of application/json responses, allowing them to be returned as XML
+ * if the accepts request header explicitly includes the
+ * "application/xml" media type before "application/json".
  * @param {object} server - Fastify instance.
  */
 async function plugin(server) {
 	server.addHook("onSend", async (req, res, payload) => {
 		/**
 		 * Ensure it does not attempt to serialise non-JSON responses,
-		 * by default Fastify sets response type to json if it has not
-		 * been explicitly defined
+		 * by default Fastify sets response type to application/json
+		 * if it has not been explicitly defined
 		 */
-		if (res.getHeader("content-type").includes("json")) {
+		if (res.getHeader("content-type").includes("application/json")) {
 			/**
 			 * If XML is the preferred response type if both XML
 			 * and JSON are declared
