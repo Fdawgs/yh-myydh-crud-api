3881945d82324d1af7711f0c5ff7e75e13a621f8
Frazer Smith <frazer.dev@outlook.com>
2022-05-12 08:23:59 +0100

    test(migrate): use `toBe` when expecting primitive literals

--- src/migrate.test.js
+++ src/migrate.test.js
@@ -57,7 +57,7 @@ describe("Migrate Script", () => {
 
 				await migrate();
 
-				expect(process.exitCode).toEqual(1);
+				expect(process.exitCode).toBe(1);
 				expect(mockMigrate).toHaveBeenCalledTimes(1);
 				expect(mockLog).toHaveBeenCalledTimes(1);
 			});
