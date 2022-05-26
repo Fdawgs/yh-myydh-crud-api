18e525108fc53ba60b8ca29220116c0206c1cdbb
Frazer Smith <frazer.dev@outlook.com>
2022-05-26 12:44:26 +0100

    fix(routes): time and timezone suffix are case-sensitive

--- src/routes/admin/access/bearer-token/schema.js
+++ src/routes/admin/access/bearer-token/schema.js
@@ -3,8 +3,12 @@ const S = require("fluent-json-schema");
 const security = [{ basicAuth: [] }];
 const tags = ["System Administration"];
 
+/**
+ * JSON Schema expects a String, `fluent-json-schema` converts this from
+ * a RegExp to a String. JSON Schema does not support Regex flags.
+ */
 const dateTimeSearchPattern =
-	/^(?:eq|ne|ge|le|gt|lt|sa|eb|ap|)\d{4}(?:-[0-1]\d-[0-3]\d(?:t(?:[0-2]\d:[0-5]\d:[0-5]\d|23:59:60)(?:\.\d+)?(?:z|[+-]\d\d(?::?\d\d)?))?)?$/im;
+	/^(?:eq|ne|ge|le|gt|lt|sa|eb|ap|)\d{4}(?:-[0-1]\d-[0-3]\d(?:T(?:[0-2]\d:[0-5]\d:[0-5]\d|23:59:60)(?:\.\d+)?(?:Z|[+-]\d\d(?::?\d\d)?))?)?$/m;
 
 const dateTimeSearchPatternExamples = [
 	"2022",
