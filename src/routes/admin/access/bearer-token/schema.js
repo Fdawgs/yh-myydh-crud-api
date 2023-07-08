642329a9c00a5a953e9a3b4b3427c94eb3c77732
Frazer Smith <frazer.dev@outlook.com>
2023-07-08 09:13:50 +0100

    style(routes): sort regex alternatives

--- src/routes/admin/access/bearer-token/schema.js
+++ src/routes/admin/access/bearer-token/schema.js
@@ -9,7 +9,7 @@ const tags = ["System administration"];
  */
 const dateTimeSearchPattern =
 	// eslint-disable-next-line security/detect-unsafe-regex
-	/^(?:eq|ne|ge|le|gt|lt|sa|eb|ap|)\d{4}-[01]\d-[0-3]\d(?:T(?:[0-2]\d:[0-5]\d:[0-5]\d|23:59:60)(?:\.\d+)?(?:Z|[+-]\d\d(?::?\d\d)?))?$/u;
+	/^(?:ap|eb|eq|ge|gt|le|lt|ne|sa|)\d{4}-[01]\d-[0-3]\d(?:T(?:[0-2]\d:[0-5]\d:[0-5]\d|23:59:60)(?:\.\d+)?(?:Z|[+-]\d\d(?::?\d\d)?))?$/u;
 
 const dateTimeSearchPatternExamples = [
 	"2022-01-13",
