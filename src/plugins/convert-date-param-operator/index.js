43a8a9193d38375a1b2a05ba104faaa994339d58
Frazer Smith <frazer.dev@outlook.com>
2023-07-18 18:31:34 +0100

    refactor(plugins): move decorator functions inside plugins

--- src/plugins/convert-date-param-operator/index.js
+++ src/plugins/convert-date-param-operator/index.js
@@ -4,43 +4,43 @@ const fp = require("fastify-plugin");
 
 /**
  * @author Frazer Smith
- * @author NextGen Healthcare
- * @description Converts date param operator to corresponding value.
- * @param {string} operator - Date param operator, in any letter case.
- * @returns {string} converted date param operator.
- */
-function convDateParamOperator(operator) {
-	switch (operator.toLowerCase()) {
-		case "ap":
-			return "=";
-		case "eb":
-			return "<";
-		case "eq":
-			return "=";
-		case "ge":
-			return ">=";
-		case "gt":
-			return ">";
-		case "le":
-			return "<=";
-		case "lt":
-			return "<";
-		case "ne":
-			return "!=";
-		case "sa":
-			return ">";
-		default:
-			return "=";
-	}
-}
-
-/**
- * @author Frazer Smith
  * @description Plugin that decorates Fastify instance with `convertDateParamOperator` function,
  * which convert date param operators to corresponding values.
  * @param {import("fastify").FastifyInstance} server - Fastify instance.
  */
 async function plugin(server) {
+	/**
+	 * @author Frazer Smith
+	 * @author NextGen Healthcare
+	 * @description Converts date param operator to corresponding value.
+	 * @param {string} operator - Date param operator, in any letter case.
+	 * @returns {string} converted date param operator.
+	 */
+	function convDateParamOperator(operator) {
+		switch (operator.toLowerCase()) {
+			case "ap":
+				return "=";
+			case "eb":
+				return "<";
+			case "eq":
+				return "=";
+			case "ge":
+				return ">=";
+			case "gt":
+				return ">";
+			case "le":
+				return "<=";
+			case "lt":
+				return "<";
+			case "ne":
+				return "!=";
+			case "sa":
+				return ">";
+			default:
+				return "=";
+		}
+	}
+
 	server.decorate("convertDateParamOperator", convDateParamOperator);
 }
 
