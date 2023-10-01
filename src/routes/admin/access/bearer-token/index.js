63ab167378cfd11916516396997f823f9256ba41
Frazer Smith <frazer.dev@outlook.com>
2023-10-01 14:45:45 +0100

    refactor(routes): use `String.slice()` over `String.substring()`

--- src/routes/admin/access/bearer-token/index.js
+++ src/routes/admin/access/bearer-token/index.js
@@ -218,11 +218,11 @@ async function route(server, options) {
 					expires.forEach((expiryDate) => {
 						let date = expiryDate;
 						const operator = server.convertDateParamOperator(
-							date.substring(0, 2)
+							date.slice(0, 2)
 						);
 
-						if (Number.isNaN(Number(date.substring(0, 2)))) {
-							date = date.substring(2, date.length);
+						if (Number.isNaN(Number(date.slice(0, 2)))) {
+							date = date.slice(2);
 						}
 
 						whereArray.push(escSq`(expires ${operator} '${date}')`);
@@ -241,11 +241,11 @@ async function route(server, options) {
 					created.forEach((createDate) => {
 						let date = createDate;
 						const operator = server.convertDateParamOperator(
-							date.substring(0, 2)
+							date.slice(0, 2)
 						);
 
-						if (Number.isNaN(Number(date.substring(0, 2)))) {
-							date = date.substring(2, date.length);
+						if (Number.isNaN(Number(date.slice(0, 2)))) {
+							date = date.slice(2);
 						}
 
 						whereArray.push(escSq`(created ${operator} '${date}')`);
@@ -266,11 +266,11 @@ async function route(server, options) {
 					lastUpdated.forEach((lastUpdatedDate) => {
 						let date = lastUpdatedDate;
 						const operator = server.convertDateParamOperator(
-							date.substring(0, 2)
+							date.slice(0, 2)
 						);
 
-						if (Number.isNaN(Number(date.substring(0, 2)))) {
-							date = date.substring(2, date.length);
+						if (Number.isNaN(Number(date.slice(0, 2)))) {
+							date = date.slice(2);
 						}
 
 						whereArray.push(
